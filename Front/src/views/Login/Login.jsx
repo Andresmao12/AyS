6cd272c82d842b6d9e1613ed03ed484f1c66eb7c
Mauro <andresmao205@gmail.com>
2025-03-15 08:06:42 -0500

    Se actualiza la vista de login

--- Front/src/views/Login/Login.jsx
+++ Front/src/views/Login/Login.jsx
@@ -1,16 +1,36 @@
-import Styles from "./Login.module.css"
+import styles from './Login.module.css'
 import { useNavigate } from "react-router-dom"
+import { FaUser, FaLock } from "react-icons/fa";
 
 const Login = () => {
 
     const navigate = useNavigate();
 
     return (
-        <main style={{display : "flex", justifyContent : "center", alignItems: "center", height : "100%", width : "100vw"}}>
+        <div className={styles.login}>
+            <form action="">
+                <h1>Bienvenido</h1>
+                <div className={styles.cajatexto}>
+                    <input type="text" placeholder="Nombre de usuario" required/>
+                    <FaUser className={styles.icon} />
+                </div>
+                <div className={styles.cajatexto}>
+                    <input type="password" placeholder="Contraseña" required></input>
+                    <FaLock className={styles.icon} />
+                </div>
+                <div className={styles.recordar}>
+                    <label>
+                        <input type="checkbox"/>
+                        <a href="#"> Recordar contraseña</a>
+                    </label>
+                </div>
+                <button type="submit" onClick={() => navigate("/formularios")}>Ingresar</button>
 
-            <button onClick={() => navigate("/formularios")}>Access</button>
-
-        </main>
+                <div className={styles.registrarCuenta}>
+                    <p>No tiene una cuenta? <a href="#">Registrarse</a></p>
+                </div>
+            </form>
+        </div>
     )
 
 }
